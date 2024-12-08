dc8b51be70c065d84b2008cf907b8486fd89b440
Sofiia Folvarochna <sofiyafolv@gmail.com>
2024-12-08 13:31:45 +0200

    wikipedia tool should work better now

--- tool_wikipedia/wikipedia_tool.py
+++ tool_wikipedia/wikipedia_tool.py
@@ -1,19 +1,23 @@
-import wikipediaapi
+from langchain_community.retrievers import WikipediaRetriever
+from .text_preprocessing import keywords_extraction
+from sentence_transformers import SentenceTransformer, util
 
-CUSTOM_USER_AGENT = "ZNO/1.0 (sofiyafolv@gmail.com)"
-wiki = wikipediaapi.Wikipedia(user_agent="ZNO/1.0 (sofiyafolv@gmail.com)", language="uk")
+retriever = WikipediaRetriever(lang="uk")
+embedding_model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
 
-def get_wikipedia_context(query: str) -> str:
-    keywords = query.split()
+def get_wikipedia_context(query):
+    keywords = keywords_extraction(query)
+    query_embedding = embedding_model.encode(query, convert_to_tensor=True)
     context = ""
-    for keyword in keywords:
-        page = wiki.page(keyword)
-        if page.exists():
-            context += f"{keyword}:\n{page.summary}\n\n"
-        else:
-            context += f"{keyword}: Не можливо знайти інформацію.\n\n"
-    
-    return context.strip()
 
-if __name__ == "__main__":
-    print(get_wikipedia_context("Конституцію України було ухвалено"))
+    for keyword in keywords:
+        docs = retriever.invoke(keyword)
+        page = docs[0]
+        page_embedding = embedding_model.encode(page.page_content, convert_to_tensor=True)
+        score = util.pytorch_cos_sim(query_embedding, page_embedding).item()
+        if score >= 0.5:
+            context += f"{keyword} (Relevance: {score:.2f}):\n{page.page_content}\n\n"
+        else:
+            context += f"{keyword} (Relevance: {score:.2f}): Не релевантна інформація.\n\n"
+
+    return context.strip()
